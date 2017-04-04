07db6cd50833184ddb82e78b15fb40822c1e07bc
Alan Arnholt <arnholtat@appstate.edu>
2017-04-04 14:34:26 -0400

    tweaks

--- Rscripts/RidgeRegression.R
+++ Rscripts/RidgeRegression.R
@@ -12,9 +12,21 @@ plot(ridge.mod, label = TRUE, xvar = "norm")
 plot(ridge.mod, label = TRUE, xvar = "lambda")
 plot(ridge.mod, label = TRUE, xvar = "dev")
 #
+print(ridge.mod)
 # Parallel computing does not do much with this example.
 #
 library(doMC)
 registerDoMC(cores = 2)
 system.time(cv.glmnet(X, y))
 system.time(cv.glmnet(X, y, parallel = TRUE))
+
+#
+#
+cvfit <- cv.glmnet(X, y, nfolds = 10)
+plot(cvfit)
+cvfit$lambda.min
+cvfit$lambda.1se
+#
+system.time(cv.glmnet(X, y, nfolds = 5))
+system.time(cv.glmnet(X, y, nfolds = 5, parallel = TRUE))
+
